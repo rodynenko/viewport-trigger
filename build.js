fbb6bb431476fe48289bb52fa49601079b3b197d
Taras Rodynenko <t.rodynenko@gmail.com>
2018-03-22 16:58:39 +0100

    add build function, modify package.json

---
+++ build.js
@@ -0,0 +1,49 @@
+const { rollup } = require('rollup');
+const filesize = require('rollup-plugin-filesize');
+const babel = require('rollup-plugin-babel');
+const uglify = require('rollup-plugin-uglify');
+
+const targets = {
+	min: 'dist/viewport-target.min.js',
+	umd: 'dist/viewport-target.js'
+};
+
+const defaultPlugins = [
+	babel({
+		babelrc: false,
+		presets: [
+			[
+				'env',
+				{
+					modules: false
+				}
+			],
+			'stage-0'
+		],
+		plugins: [
+			'external-helpers'
+		]
+	}),
+	filesize()
+];
+
+function build(type) {
+	const plugins = type === 'min' ? defaultPlugins.concat(uglify()) : defaultPlugins;
+
+	return rollup({
+		input: 'src/viewport-trigger.js',
+		plugins
+	}).then(bundle =>
+		bundle.write({
+			file: targets[type],
+			format: type === 'min' ? 'iife' : 'umd',
+			name: 'ViewportTrigger'
+		}));
+}
+
+function generateBundle() {
+	return Promise.all([build('umd'), build('min')])
+		.catch(err => console.log(err));
+}
+
+generateBundle();
